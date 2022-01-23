c11d5b9b16578205f346a766981cb809009330e4
ChinghungTan <92668959+ChinghungTan@users.noreply.github.com>
2022-01-22 21:51:55 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,79 @@
+'''
+Melanocytic nevi (nv)
+Melanoma (mel)
+Benign keratosis-like lesions (bkl)
+Basal cell carcinoma (bcc) 
+Actinic keratoses (akiec)
+Vascular lesions (vas)
+Dermatofibroma (df)
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from glob import glob
+from PIL import Image
+
+###########################################
+#METHOD 1: Read files using file name from the csv and add corresponding 
+#image in a pandas dataframe along with labels. 
+#This requires lot of memory to hold all thousands of images. 
+#Use datagen if you run into memory issues. 
+
+skin_df = pd.read_csv('HAM10000/HAM10000_metadata.csv')
+
+#Now time to read images based on image ID from the CSV file
+#This is the safest way to read images as it ensures the right image is read for the right ID
+image_path = {os.path.splitext(os.path.basename(x))[0]: x
+                     for x in glob(os.path.join('HAM10000/', '*', '*.jpg'))}
+
+#Define the path and add as a new column
+skin_df['path'] = skin_df['image_id'].map(image_path.get)
+#Use the path to read images.
+skin_df['image'] = skin_df['path'].map(lambda x: np.asarray(Image.open(x).resize((32,32))))
+
+print(skin_df['dx'].value_counts())
+
+n_samples = 5  # number of samples for plotting
+# Plotting
+fig, m_axs = plt.subplots(7, n_samples, figsize = (4*n_samples, 3*7))
+for n_axs, (type_name, type_rows) in zip(m_axs, 
+                                         skin_df.sort_values(['dx']).groupby('dx')):
+    n_axs[0].set_title(type_name)
+    for c_ax, (_, c_row) in zip(n_axs, type_rows.sample(n_samples, random_state=1234).iterrows()):
+        c_ax.imshow(c_row['image'])
+        c_ax.axis('off')
+
+# Reorganize data into subfolders based on their labels
+#then use keras flow_from_dir or pytorch ImageFolder to read images with 
+#folder names as labels
+
+#Sort images to subfolders first 
+import pandas as pd
+import os
+import shutil
+
+# Dump all images into a folder and specify the path:
+data_dir = os.getcwd() + "/all_images/"
+
+# Path to destination directory where we want subfolders
+dest_dir = os.getcwd() + "/recorganized/"
+
+# Read the csv file containing image names and corresponding labels
+skin_df2 = pd.read_csv('HAM10000/HAM10000_metadata.csv')
+print(skin_df['dx'].value_counts())
+
+label=skin_df2['dx'].unique().tolist()  #Extract labels into a list
+label_images = []
+
+
+# Copy images to new folders
+for i in label:
+    os.mkdir(dest_dir + str(i) + "/")
+    sample = skin_df2[skin_df2['dx'] == i]['image_id']
+    label_images.extend(sample)
+    for id in label_images:
+        shutil.copyfile((data_dir + "/"+ id +".jpg"), (dest_dir + i + "/"+id+".jpg"))
+    label_images=[]    
+
+#Now we are ready to work with images in subfolders
