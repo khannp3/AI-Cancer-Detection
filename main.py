7578afdb180c127f31e046e1da5a935140befb06
Pragya Khanna <64930593+khannp3@users.noreply.github.com>
2022-01-22 22:06:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,6 @@ normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 # Load the image into the array
 data[0] = normalized_image_array
 
-# run the inference
+# run the inference 
 prediction = model.predict(data)
 print(prediction)
