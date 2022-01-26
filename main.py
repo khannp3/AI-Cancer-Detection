e19e3b71aa76f4acc1dfc0267b36e9388283ef29
Pragya Khanna <64930593+khannp3@users.noreply.github.com>
2022-01-25 19:31:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 import os
 print(os.getcwd())
 # Load the model
-model = load_model('keras_model.h5')
+model = load_model('keras_model.h5')  
 
 # Create the array of the right shape to feed into the keras model
 # The 'length' or number of images you can put into the array is
